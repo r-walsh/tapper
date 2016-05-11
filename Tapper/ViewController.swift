081aa24da90561597c8b286de1bd6fd1d42c2aec
Ryan Walsh <ryan@thehatrack.net>
2016-05-11 09:44:09 -0500

    initial

--- Tapper/ViewController.swift
+++ Tapper/ViewController.swift
@@ -9,17 +9,55 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    // Initial State
+    var numberOfTaps = 0
+    var tapGoal = 1
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    // Initally shown
+    @IBOutlet weak var logoImg: UIImageView!
+    @IBOutlet weak var howManyTapsTxt: UITextField!
+    @IBOutlet weak var playBtn: UIButton!
+    
+    // Initially hidden
+    @IBOutlet weak var tapBtn: UIButton!
+    @IBOutlet weak var tapsLbl: UILabel!
+    
+    
+    func resetState() -> Void {
+        tapBtn.hidden = true
+        tapsLbl.hidden = true
+        
+        logoImg.hidden = false
+        howManyTapsTxt.text? = ""
+        howManyTapsTxt.hidden = false
+        playBtn.hidden = false
+        
+        numberOfTaps = 0
+        tapGoal = 1
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    // MARK actions
+    
+    @IBAction func onPlayBtnPressed( sender: UIButton! ) -> Void {
+        logoImg.hidden = true
+        howManyTapsTxt.hidden = true
+        playBtn.hidden = true
+        
+        tapGoal = Int(howManyTapsTxt.text!)!
+        tapBtn.hidden = false
+        tapsLbl.text = "\( numberOfTaps )"
+        tapsLbl.hidden = false
     }
-
-
+    
+    @IBAction func onTapBtnTap( sender: UIButton! ) -> Void {
+        numberOfTaps += 1
+        tapsLbl.text = "\( numberOfTaps )"
+        
+        if numberOfTaps >= tapGoal {
+            resetState()
+        }
+    }
+    
 }
 
